aa67ccd51549165cfaa534961596b1f9c382ce73
lee747 <lee747@purdue.edu>
2014-02-18 13:24:44 -0500

    state machine basics working, refere examples/tools/DotParser.

--- ns-3-dev/examples/tools/DotParser.cc
+++ ns-3-dev/examples/tools/DotParser.cc
@@ -1,19 +1,51 @@
 #include "ns3/dotParser.h"
+#include "ns3/stateMachine.h"
 
 using namespace ns3;
 
 int main(int argc, char **argv) {
     const char *filename = "filename.txt";
-    std::string state = "ESTB";
+    std::string initState = "CLOSED";
+    std::string toState = "CLOSE_WAIT";
     if (argc > 1) {
         std::cout << "test file:[" << argv[1] << "]" << std::endl;
         filename = argv[1];
     }
     if (argc > 2) {
-        state = argv[2];
+        initState = argv[2];
+    }
+    if (argc > 3) {
+        toState = argv[3];
     }
     DotParser *dt = new DotParser();
     dt->parseGraph(filename);
     dt->print_graph();
-    dt->getLegalTransision(std::string(state));
+    StateMachine *m = new StateMachine();
+    dt->BuildStateMachine(m);
+    
+    m->Start(initState);
+    TrSet valid = m->GetValidTransitions(m->GetCurrentState());
+    if (!valid.empty()) {
+        Transition tr = *valid.begin();
+        m->MakeTransition(tr);
+        m->Print();
+    } else {
+        std::cout << "No possible Transition from " << initState << std::endl;
+    }
+
+    TrSet toGo = m->GetTransitionsTo(toState);
+    if (!toGo.empty()) {
+        Transition tr = *toGo.begin();
+        m->MakeTransition(tr);
+        m->Print();
+    } else {
+        std::cout << "No Transition to Go " << toState << std::endl;
+    }
+    
+    TrSet invalid = m->GetInvalidTransitions(m->GetCurrentState());
+    Transition tr = *invalid.begin();
+    m->MakeTransition(tr);
+
+    m->Print();
+    
 }
