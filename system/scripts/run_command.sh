e660db4617836592289b33642ea27bf9f2ff70bf
Samuel Jero <sjero@purdue.edu>
2014-06-30 22:37:53 -0400

    Fix run_command.sh to work better again

--- system/scripts/run_command.sh
+++ system/scripts/run_command.sh
@@ -1,29 +1,31 @@
 #!/bin/bash
-./arp.sh 13 > ./run_arp.sh
-chmod 755 ./run_arp.sh
-#echo "setting VM arp table"
-#./run_arp.sh
 
 #NS3 debugging
 #NS_LOG='*=level_all|prefix_func|prefix_time'
 #export NS_LOG
 
-#username=$(whoami)
-username=lee747
+username=$(whoami)
+hostname=$(hostname)
 
 #Figure out what the absolute path is... this is a terrible hack
 NS3_PATH="/home/$username/ns-3-dev"
-if [ $username = "lee747" ]; then
-NS3_PATH="/home/$username/GIT/sjero-turret-ns3" #cloud12
-#NS3_PATH="/home/$username/GIT/ns-3-dev" #cloud12
-#NS3_PATH="/scratch/lee747/turret/ns-3-dev" #sound
+if [ $username = "lee747" ] && [ $hostname = "cloud12.cs.purdue.edu" ]; then
+NS3_PATH="/home/lee747/GIT/sjero-turret-ns3"
 fi
-if [ $username = "sjero" ]; then
-NS3_PATH="/scratch2/sjero/ns-3-dev" #ocean1
+if [ $username = "lee747" ] && [ $hostname = "sound.cs.purdue.edu" ]; then
+NS3_PATH="/scratch/lee747/turret/ns-3-dev"
 fi
-if [ $username = "root" ]; then
-NS3_PATH="/home/sjero/turret/ns-3-dev" #cloud15
-NS3_PATH="/home/lee747/GIT/ns-3-dev" #cloud12
+if [ $username = "root" ] && [ $hostname = "cloud12.cs.purdue.edu" ]; then
+NS3_PATH="/home/lee747/GIT/ns-3-dev" #Runs as root...
+fi
+if [ $username = "sjero" ] && [ $hostname = "sound.cs.purdue.edu" ]; then
+NS3_PATH="/scratch/sjero/ns-3-dev"
+fi
+if [ $username = "sjero" ] && [ $hostname = "ocean1.cs.purdue.edu" ]; then
+NS3_PATH="/scratch2/sjero/ns-3-dev"
+fi
+if [ $username = "root" ] && [ $hostname = "cloud15.cs.purdue.edu" ]; then
+NS3_PATH="/home/sjero/turret/ns-3-dev" #Runs as root...
 fi
 echo "NS-3 Path: $NS3_PATH"
 
