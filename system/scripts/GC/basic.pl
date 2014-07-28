5ac7cdb5870b34b27821a99dbf20e4dd022b6b4e
lee747 <lee747@purdue.edu>
2014-07-28 11:08:04 -0400

    debug print

--- system/scripts/GC/basic.pl
+++ system/scripts/GC/basic.pl
@@ -1,4 +1,4 @@
-#!/usr/bin/perl
+#!/usr/bin/env perl
 
 ########################
 # STATELESS ATTACKS
@@ -10,43 +10,35 @@ use Switch;
 my $str = "";
 my $DEBUG = 0;
 
-######################
-# Some static setting - make it configurable later
-######################
-my $clientip = "10.1.2.2";
-my $serverip = "10.1.2.3";
-my $malip = "10.1.2.1";
-my $clientport= 5555;
-my $serverport= 80;
-my $malport = 5556;
-my $defaultwindow=20000;
+my $system = "TCP";
 
 ######################
 # LOAD MSG TYPES
 ######################
 
 use lib ("../Gatling/");
-#require MsgParse;
 require Strategy;
 require GatlingConfig;
+require Utils;
 
-GatlingConfig::systemTCP();
+$GatlingConfig::systemname = $system;
+GatlingConfig::setSystem();
 my $fieldsPerMsgRef 	= Strategy::parseMessage();
 my $msgTypeRef      	= Strategy::getMsgTypeRef();
 my $msgNameRef = Strategy::getMsgNameRes();
-
-# %msgFlenList - 
 my $fieldRef = Strategy::getFlenList(); 
-# my $fieldRef = Strategy::getMsgFlenList(); 
 
 # for each types (e.g. int8_t, bool, etc.)
 my $coarseStrategyRef = Strategy::getCoarseStrategyList();
 my $fineStrategyRef = Strategy::getFineStrategyList();
 
+#Get perf from cmd line
+my $perfInput="";
 foreach my $arg (@ARGV) {
-    $str .= "$arg";
+	$perfInput .= "$arg";
 }
 
+#Get other feedback from stdin
 my @feedback;
 my $gotany = 0;
 
@@ -68,31 +60,86 @@ while (<STDIN>) {
     DEBUGPRINT("got from stdin: $line-----------");
 }
 
+
+
+# Analyize our perf score, if we got one
+if(length($perfInput)> 0){
+	my $benign = getBenignPerf();
+	chomp($perfInput);
+	my @token = split(':', $perfInput);
+	if ( $#token < 1 or $token[0] =~ /^\/\// or $token[0] =~ /^#/ ) {
+		#skip
+	}else{
+		my $perf = $token[2];
+		my $strategy = $token[1];
+		my $res   = $token[3];
+		my $score = Utils::computeAttackScore($perf,$res);
+
+		#Save Perf Score
+		my $f = open PERF, "+>>", $GatlingConfig::perfMeasured;
+		if($f){
+			print PERF "$strategy,$perf,$res,$benign\n";
+			close(PERF);
+		}
+
+		#Check for Attack
+		if($benign > 0 and isAttack($score, $benign)){
+			$f = open LEARNED, "+>>", $GatlingConfig::newlyLearned;
+			if($f){
+				print LEARNED "FOUND $strategy,$perf,$res,$benign\n";
+				close(LEARNED);
+			}
+		}
+	}
+}
+
+#Figure out more/new strategies
 my $weight=0;
 my @strArray;
-
 my $short = 0;
 if ($gotany == 0) {
-### WHEN NO FEEDBACK IS GIVEN
+    #No feedback---system start
     my $i = 0;
     foreach my $msgType (@$msgNameRef) {
         my $prefix = "$weight:*?*?$msgType";
         if ($msgType eq "BaseMessage") {
             push (@strArray, "$prefix NONE 0");
+            push (@strArray, "$prefix NONE 1");
+            push (@strArray, "$prefix NONE 2");
             $weight++;
         } else {
-            push(@strArray, "$prefix INJECT t=10 0 $clientip $serverip 0=$clientport 1=$serverport 2=111 5=5 10=$defaultwindow");
-            push(@strArray, "$prefix INJECT t=10 0 $serverip  $clientip 0=$serverport 1=$clientport 2=111 5=5 10=$defaultwindow");
-            push(@strArray, "$prefix WINDOW w=$defaultwindow t=10 $clientip $serverip $clientport $serverport 5");
-            push(@strArray, "$prefix WINDOW w=$defaultwindow t=10 $serverip $clientip $serverport $clientport 5");
+		if($GatlingConfig::systemname == "DCCP"){
+		    my $size=0;
+		    if($msgType == "BaseMessage"){
+			    $size = 3;
+		    }elsif($msgType == "Data"){
+			    $size = 4;
+		    }elsif($msgType == "Request"){
+			    $size = 5;
+		    }elsif($msgType == "Reset"){
+			    $size = 7;
+		    }else{
+			    $size = 6;
+		    }
+		    push(@strArray, "$prefix INJECT t=10 0 $GatlingConfig::clientip $GatlingConfig::serverip 0=$GatlingConfig::clientport 1=$GatlingConfig::serverport 2=$size 6=1 11=111");
+		    push(@strArray, "$prefix INJECT t=10 0 $GatlingConfig::serverip  $GatlingConfig::clientip 0=$GatlingConfig::serverport 1=$GatlingConfig::clientport 2=$size 6=1 11=111");
+		    push(@strArray, "$prefix WINDOW w=$GatlingConfig::defaultwindow t=10 $GatlingConfig::clientip $GatlingConfig::serverip 0=$GatlingConfig::clientport 1=$GatlingConfig::serverport 2=$size 6=1");
+	            push(@strArray, "$prefix WINDOW w=$GatlingConfig::defaultwindow t=10 $GatlingConfig::serverip $GatlingConfig::clientip 0=$GatlingConfig::serverport 1=$GatlingConfig::clientport 2=$size 6=1");
+		}elsif($GatlingConfig::systemname == "TCP"){
+		    push(@strArray, "$prefix INJECT t=10 0 $GatlingConfig::clientip $GatlingConfig::serverip 0=$GatlingConfig::clientport 1=$GatlingConfig::serverport 2=111 5=5 10=$GatlingConfig::defaultwindow");
+		    push(@strArray, "$prefix INJECT t=10 0 $GatlingConfig::serverip  $GatlingConfig::clientip 0=$GatlingConfig::serverport 1=$GatlingConfig::clientport 2=111 5=5 10=$GatlingConfig::defaultwindow");
+		    push(@strArray, "$prefix WINDOW w=$GatlingConfig::defaultwindow t=10 $GatlingConfig::clientip $GatlingConfig::serverip 0=$GatlingConfig::clientport 1=$GatlingConfig::serverport 5=5");
+	            push(@strArray, "$prefix WINDOW w=$GatlingConfig::defaultwindow t=10 $GatlingConfig::serverip $GatlingConfig::clientip 0=$GatlingConfig::serverport 1=$GatlingConfig::clientport 5=5");
+		}
         }
-       if ($short == 1 && $i == 1) { # XXX - to keep it short
+       if ($short == 1 && $i == 1) { # debugging option - to keep it short
            last;
        }
         $i++;
     }
 
 } else {
+    #Examine feedback and suggest strategies
     foreach my $line (@feedback) {
         if ($short == 1) {
             last;
@@ -155,6 +202,7 @@ sub strategyCompose {
             DEBUGPRINT("fine strategy for $msg,$state");
             my $weight = 5;
             my $prefix = "$weight:$state?$msg";
+            push (@strArray, "$prefix BURST 1.0");
 
             my $msgType = $msgTypeRef->{$msg};
             my $fields = $fieldsPerMsgRef->{$msgType};
@@ -175,22 +223,50 @@ sub strategyCompose {
     }
 }
 
+sub getBenignPerf{
+	my $total = 0;
+	my $num = 0;
+	my $res = open PERF, "<", $GatlingConfig::perfMeasured;
+        if ($res) {
+                while ( my $line = <PERF> ) {
+                        chomp($line);
+                        my @token = split(',', $line );
+                        if ( $#token < 1 or $token[0] =~ /^\/\// or $token[0] =~ /^#/ ) {
+                                next;
+                        }
+                        my $perf = $token[1];
+                        my $strategy = $token[0];
+                        my $res   = $token[2];
+
+			if($strategy =~/ NONE /){
+				$total += Utils::computeAttackScore($perf,$res);
+				$num +=1;
+			}
+                }
+                close PERF;
+        }
+
+	if($num==0){
+		return 0;
+	}
+	return $total/$num;
+}
+
+sub isAttack
+{
+	my $val=shift;
+	my $ref=shift;
+
+	if($val > 1.5*$ref){ #greater than 1.5 times reference
+		return 1;
+	}
+	if($val < 0.5*$ref){ #less than 0.5 times reference
+		return 1;
+	}
+	return 0;
+}
+
+# Dump new strategies to stdout
 foreach my $strategy (@strArray) {
     print "$strategy\n";
 }
-
-#foreach my $typename (keys %{$msgTypeRef}) {
-#    print "$typename\n";
-#}
-#
-#
-#foreach my $typename (keys %{$coarseStrategyRef}) {
-#    print "$typename\n";
-#}
-
-#foreach my $key (keys %{$fieldsPerMsgRef}) {
-#    my $arrref = $fieldsPerMsgRef->{$key};
-#    foreach my $arritem (@{$arrref}) {
-#    print "$key $arritem\n";
-#    }
-#}
